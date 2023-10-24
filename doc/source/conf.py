79d48dd63f8eb3d4944821a0bd7ea63367df0ad8
LeChat76 <cedrik76@msn.com>
2023-10-24 23:51:21 +0200

    .

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,4 +34,4 @@ language = 'en'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['images']
+# html_static_path = ['images']
